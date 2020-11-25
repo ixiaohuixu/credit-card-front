02b0db91d7e64e9f7e0b8d634a34f5fbdce58b20
徐晓慧 <13811456865@139.com>
2020-11-25 17:39:41 +0800

    初始化代码

---
+++ src/router/home/index.js
@@ -0,0 +1,5 @@
+export default {
+    path:'/',
+    name:'/',
+    component:() => import('@/views/home/index')
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+import homeRoute from './home/index'
 
-const routes = [
+/*const routes = [
   {
     path: '/',
     name: 'Home',
@@ -13,13 +14,15 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/!* webpackChunkName: "about" *!/ '../views/About.vue')
   }
-]
+]*/
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes:[
+      homeRoute
+  ]
 })
 
 export default router
